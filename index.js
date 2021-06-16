42f5624d8ff52d21505ef4c6ab3cb832740d3132
shahinooriact <shahinooriact@gmail.com>
2021-06-16 13:07:46 +0600

    server restarted...

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 5055
+const port = process.env.PORT || 5500
 app.use(cors());
 app.use(bodyParser.json());
 
